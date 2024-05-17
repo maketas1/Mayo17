8829392fc98f9c9bc5d7acfbdd94eba6a553eb8c
Ángel Gallardo <angelgallardoturpin@gmail.com>
2024-05-17 12:31:36 +0200

    guardia

---
+++ src/app/_servicios/mi-guardia.guard.ts
@@ -0,0 +1,28 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { LoginService } from './login.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { entorno } from '../_enviroment/emtorno';
+
+export const miGuardiaGuard: CanActivateFn = (route, state) => {
+  const loginService = inject(LoginService);
+  const router = inject(Router);
+  const jwtHelper = inject(JwtHelperService);
+
+  let rpta = loginService.estasLogeado();
+
+  if(!rpta) {
+    console.log('no esta logeado-false')
+    loginService.cerrarSesion();
+    return false;
+  } else {
+    let token = sessionStorage.getItem(entorno.TOKEN_NAME);
+    if(!jwtHelper.isTokenExpired(token)) {
+      console.log("no ha expirado el token")
+      return true;
+    } else {
+      loginService.cerrarSesion();
+      return false;
+    }
+  }
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ContenidoComponent } from './contenido/contenido.component';
+import { miGuardiaGuard } from './_servicios/mi-guardia.guard';
 
 export const routes: Routes = [
     {"path":"", redirectTo:"/login", pathMatch:"full"},
     {"path":"login", component: LoginComponent},
-    {"path":"contenido", component: ContenidoComponent}
+    {"path":"contenido", component: ContenidoComponent, canActivate:[miGuardiaGuard]}
 ];
